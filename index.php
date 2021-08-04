39bad04a257e9148bfb6306cbe0aab2f39979e81
Dhiraj mokal <88071574+dhirajmokal13@users.noreply.github.com>
2021-08-04 07:18:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || 
+   $_SERVER['HTTPS'] == 1) ||  
+   isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&   
+   $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
+{
+   $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+   header('HTTP/1.1 301 Moved Permanently');
+   header('Location: ' . $redirect);
+   exit();
+}
+
+
 session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     header('location: Home.php');
