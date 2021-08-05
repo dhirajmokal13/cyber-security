0a15d5aac01d6600d66bf03ed42aa81bbed4e8c8
Dhiraj mokal <88071574+dhirajmokal13@users.noreply.github.com>
2021-08-05 18:06:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>login for cyber-security and ethical-hacking</title>
     <style>
         * {
             padding: 0;
